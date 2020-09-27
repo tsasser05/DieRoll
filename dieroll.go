429d13e75654197866cd369bf5208184e5967000
Tom Sasser <tom_sasser@cable.comcast.com>
2020-09-27 23:35:41 +0300

    Added tests

--- dieroll.go
+++ dieroll.go
@@ -32,15 +32,15 @@ func contains(valid [7]int, max int) bool {
 
 /*******************************************************
 *
-* init()
+* Init()
 *
 *
 *******************************************************/
 
-func init() {
+func Init() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
-} // init
+} // Init
 
 /*******************************************************
 *
@@ -57,7 +57,7 @@ func randomInt(max int) int {
 } // randomInt()
 
 /*******************************************************
-* roll (max, num)
+* Roll (max, num)
 *
 * Rolls a number of times equal to num.  Each result is
 * between 1 and max.
@@ -77,9 +77,6 @@ func randomInt(max int) int {
 func Roll(max int, num int) int {
 	sum := 0
 
-	// yeah, i know
-	debug := 0
-
 	// max must be 5 for d4, 7 for d6, 9 for d8, 11 for d10, 13 for d12 and 21 for d20
 	// max += 1
 	// Limited to the following:
@@ -98,41 +95,75 @@ func Roll(max int, num int) int {
 	if num == 1 {
 		sum = randomInt(max)
 
-		if debug > 0 {
-			fmt.Println("dieroll.Roll() number rolled is ", sum)
-		} // if
-
 	} else if num > 1 {
 		count := 0
 
 		for count < num {
 			new_num := randomInt(max)
-
-			if debug > 0 {
-				fmt.Println("dieroll.Roll() number rolled is ", new_num)
-			} // if
-
 			sum += new_num
 			count += 1
-
-			if debug > 0 {
-				fmt.Println("dieroll.Roll() sum is ", sum)
-			} // debug
-
-		} // for
-
-		if debug > 0 {
-			fmt.Println("dieroll.Roll() final sum is ", sum)
-		} // debug
+		}
 
 	} else {
 		fmt.Println("dieroll.Roll: Incorrect number of dice passed to function: ", num)
 		os.Exit(3)
-	} // if
+	}
 
 	return sum
 
-} // roll()
+} // Roll()
+
+/*******************************************************
+* Rolldice (max, num)
+*
+* Rolls a number of times equal to num.  Each result is
+* between 1 and max.
+*
+* Returns a list of all results.
+*
+* Max must be equal to 4, 6, 8, 10, 12, 20 or 100.  The
+* function will handle the details for rand.Intn itself.
+*
+*******************************************************/
+
+func Rolldice(max int, num int) []int {
+	results := make([]int, num)
+
+	// max must be 5 for d4, 7 for d6, 9 for d8, 11 for d10, 13 for d12 and 21 for d20
+	// max += 1
+	// Limited to the following:
+	var valid = [7]int{4, 6, 8, 10, 12, 20, 100}
+
+	valid_check := contains(valid, max)
+
+	if valid_check == false {
+		fmt.Println("dieroll().Roll(): max value = ", max, "is not correct value.")
+		os.Exit(3)
+	} // if
+
+	// Increment max by one for randomInt calculation.
+	max += 1
+
+	if num == 1 {
+		results[0] = randomInt(max)
+
+	} else if num > 1 {
+		count := 0
+
+		for count < num {
+			roll := randomInt(max)
+			results[count] = roll
+			count += 1
+		}
+
+	} else {
+		fmt.Println("dieroll.Roll: Incorrect number of dice passed to function: ", num)
+		os.Exit(3)
+	}
+
+	return results
+
+} // Rolldice()
 
 /*******************************************************
 *
@@ -145,9 +176,6 @@ func Roll(max int, num int) int {
 func RollStat() int {
 	rolls := make([]int, 4, 4)
 
-	// yeah, i know
-	debug := 0
-
 	// d6 uses 7 in randomInt
 	max := 7
 	i := 0
@@ -158,22 +186,8 @@ func RollStat() int {
 		i++
 	}
 
-	if debug > 0 {
-		fmt.Println("Content of rolls prior to sorting:  ", rolls)
-	} // if
-
 	sort.Ints(rolls)
-
-	if debug > 0 {
-		fmt.Println("Content of rolls after sorting:  ", rolls)
-	} // if
-
 	sliced_rolls := rolls[1:]
-
-	if debug > 0 {
-		fmt.Println("Content of sliced_rolls after pruning:  ", sliced_rolls)
-	} // if
-
 	sum := 0
 
 	for _, roll := range sliced_rolls {
